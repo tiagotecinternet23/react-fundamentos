69a2f2f69fa86546d187186598e55ab9a3c97dae
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-29 11:17:32 -0300

    Componente único no App

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,36 @@ function App() {
                   situação. */}
 
       <main>
-        <h2>Teste</h2>
+        <section>
+          <h2>Conteúdo da aplicação</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+            labore? Officia quae quo a quas excepturi distinctio sint voluptas
+            labore iste veniam possimus facere adipisci sit repellat, voluptate,
+            expedita aspernatur.
+          </p>
+
+          <article>
+            <h3>Artigo 1</h3>
+            <p>Conteúdo do artigo 1....</p>
+          </article>
+
+          <article>
+            <h3>Artigo 2</h3>
+            <p>Conteúdo do artigo 2....</p>
+          </article>
+
+          <article>
+            <h3>Artigo 3</h3>
+            <p>Conteúdo do artigo 3....</p>
+          </article>
+        </section>
       </main>
+
+      <footer>
+        <h2>Rodapé da aplicação</h2>
+      </footer>
     </>
   );
 }
