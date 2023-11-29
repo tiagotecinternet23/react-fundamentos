2eab51eeac4d6afe6befa5b4153b17506914ffbc
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-29 11:41:57 -0300

    Componente Cabecalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-function App() {
-  // Comentário JS de uma linha CTRL ;
-  /* Comentário JS de várias linhas */
+import Cabecalho from "./Cabecalho";
 
+function App() {
   return (
     <>
-      <header>
-        <h1>Olá React! 😎</h1>
-        <hr />
-        <nav>
-          <a href="">Home</a>
-          <a href="">Produtos</a>
-          <a href="">Contato</a>
-        </nav>
-      </header>
-
-      {/* Comentário no meio do JSX
-                  É normal ficar entre {} nesta
-                  situação. */}
+      <Cabecalho />
 
       <main>
         <section>
---
+++ src/Cabecalho.jsx
@@ -0,0 +1,15 @@
+function Cabecalho() {
+  return (
+    <header>
+      <h1>Olá React! 😎</h1>
+      <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Produtos</a>
+        <a href="">Contato</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Cabecalho;
