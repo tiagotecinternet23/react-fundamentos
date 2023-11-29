4276556f47045ea9436293729329d5fbd800de62
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-29 10:53:05 -0300

    Uso de fragmento <></>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App(){
-  return <h1>Olá React! 😎</h1>
+  return <>
+            <header>
+              <h1>Olá React! 😎</h1>
+            </header>
+
+            <main>
+              <h2>Teste</h2>
+            </main>
+          </>  
 }
 
 export default App
