5ce2b0e12456df992cd7c68f68c409a51f0ef4a3
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-29 11:44:34 -0300

    Exercício Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
+import Rodape from "./Rodape";
 
 function App() {
   return (
     <>
       <Cabecalho />
-
-      <main>
-        <section>
-          <h2>Conteúdo da aplicação</h2>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-            labore? Officia quae quo a quas excepturi distinctio sint voluptas
-            labore iste veniam possimus facere adipisci sit repellat, voluptate,
-            expedita aspernatur.
-          </p>
-
-          <article>
-            <h3>Artigo 1</h3>
-            <p>Conteúdo do artigo 1....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 2</h3>
-            <p>Conteúdo do artigo 2....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 3</h3>
-            <p>Conteúdo do artigo 3....</p>
-          </article>
-        </section>
-      </main>
-
-      <footer>
-        <h2>Rodapé da aplicação</h2>
-      </footer>
+      <Conteudo />
+      <Rodape />
     </>
   );
 }
---
+++ src/Conteudo.jsx
@@ -0,0 +1,33 @@
+function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo da aplicação</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+          labore? Officia quae quo a quas excepturi distinctio sint voluptas
+          labore iste veniam possimus facere adipisci sit repellat, voluptate,
+          expedita aspernatur.
+        </p>
+
+        <article>
+          <h3>Artigo 1</h3>
+          <p>Conteúdo do artigo 1....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 2</h3>
+          <p>Conteúdo do artigo 2....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 3</h3>
+          <p>Conteúdo do artigo 3....</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Conteudo;
