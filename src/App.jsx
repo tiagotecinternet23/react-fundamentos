d5c065c11326e45931cbd50c31c614b9a4883fc2
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-29 11:10:42 -0300

    Formatação com Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-function App(){
-  
+function App() {
   // Comentário JS de uma linha CTRL ;
   /* Comentário JS de várias linhas */
 
-  return <>
-            <header>
-              <h1>Olá React! 😎</h1>
-            </header>
+  return (
+    <>
+      <header>
+        <h1>Olá React! 😎</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
 
-                { /* Comentário no meio do JS
+      {/* Comentário no meio do JSX
                   É normal ficar entre {} nesta
-                  situação. */ }
-                  
-            <main>
-              <h2>Teste</h2>
-            </main>
-          </>  
+                  situação. */}
+
+      <main>
+        <h2>Teste</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
