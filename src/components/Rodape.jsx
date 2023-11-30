249f181857ffbb2bddd268f9a3b941e9523be762
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-30 09:32:57 -0300

    CSS inline via objeto JS

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,6 +1,11 @@
 function Rodape() {
   return (
-    <footer>
+    <footer
+      style={{
+        backgroundColor: "lightblue",
+        padding: "2%",
+      }}
+    >
       <h2>Rodapé da aplicação</h2>
     </footer>
   );
