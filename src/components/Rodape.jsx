cdc485122ae61f9094c7a1a5d02f816779b2abb6
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-30 10:34:18 -0300

    Classes globais via className

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -9,6 +9,10 @@ function Rodape() {
   return (
     <footer style={exemplosEstilos}>
       <h2 style={{ textAlign: "center" }}>Rodapé da aplicação</h2>
+      <p>Fundamentos de React</p>
+      <p>
+        Experimentando <b className="sombra">classes</b>
+      </p>
     </footer>
   );
 }
