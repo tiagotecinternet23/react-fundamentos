6780daf30c0194fb488e7e4d2a1913f417e5f5ae
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-30 09:03:31 -0300

    Componente Menu

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,13 +1,11 @@
+import Menu from "./Menu";
+
 function Cabecalho() {
   return (
     <header>
       <h1>Olá React! 😎</h1>
       <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Produtos</a>
-        <a href="">Contato</a>
-      </nav>
+      <Menu />
     </header>
   );
 }
