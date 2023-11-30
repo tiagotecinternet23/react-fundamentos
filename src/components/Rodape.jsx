613351eb71c292bbfd2de08773bd21f3a716a47b
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-30 09:43:23 -0300

    Update Rodape.jsx

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,12 +1,14 @@
 function Rodape() {
+  const exemplosEstilos = {
+    backgroundColor: "lightblue",
+    padding: "2%",
+    color: "blue",
+    border: "solid 4px #f00",
+  };
+
   return (
-    <footer
-      style={{
-        backgroundColor: "lightblue",
-        padding: "2%",
-      }}
-    >
-      <h2>Rodapé da aplicação</h2>
+    <footer style={exemplosEstilos}>
+      <h2 style={{ textAlign: "center" }}>Rodapé da aplicação</h2>
     </footer>
   );
 }
