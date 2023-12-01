d326201d484a827aaab616da0c379fac77172b80
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-01 10:01:07 -0300

    StyledConteudo

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,24 +1,36 @@
+/* Conteudo.jsx */
 import Artigo from "./Artigo";
+import styled from "styled-components";
+
+const StyledConteudo = styled.main`
+  width: 90vw;
+  margin: 1rem auto;
+  background-color: aliceblue;
+  padding: 1rem;
+  box-shadow: darkblue 0 0 1px inset;
+
+  h2,
+  p {
+    padding: 0.2rem 0;
+  }
+`;
 
 function Conteudo() {
   return (
-    <main>
-      <section>
-        <h2>Conteúdo da aplicação</h2>
+    <StyledConteudo>
+      <h2>Conteúdo da aplicação</h2>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-          labore? Officia quae quo a quas excepturi distinctio sint voluptas
-          labore iste veniam possimus facere adipisci sit repellat, voluptate,
-          expedita aspernatur.
-        </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        labore? Officia quae quo a quas excepturi distinctio sint voluptas
+        labore iste veniam possimus facere adipisci sit repellat, voluptate,
+        expedita aspernatur.
+      </p>
 
-        {/* Reaproveitamento de Componentes */}
-        <Artigo />
-        <Artigo />
-        <Artigo />
-      </section>
-    </main>
+      <Artigo />
+      <Artigo />
+      <Artigo />
+    </StyledConteudo>
   );
 }
 
