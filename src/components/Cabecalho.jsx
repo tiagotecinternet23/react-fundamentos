f876b55f338f27e2ec51d33e40aaea513c4b524b
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-01 09:28:25 -0300

    StyledCabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -7,6 +7,17 @@ import styled from "styled-components";
 const StyledCabecalho = styled.header`
   background-color: black;
   color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+
+  hr {
+    width: 50%;
+    background-color: pink;
+    border: none;
+    height: 4px;
+    margin: 8px auto;
+  }
 `;
 
 function Cabecalho() {
