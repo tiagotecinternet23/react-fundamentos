145da104b05052d1e5393db97bf7b57f5c402e7a
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-01 09:16:07 -0300

    Aplicação de Styled Component no header

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,21 @@
+/* Cabecalho.jsx */
 import Menu from "./Menu";
+
+/* Importação da lib Styled Components */
+import styled from "styled-components";
+
+const StyledCabecalho = styled.header`
+  background-color: black;
+  color: white;
+`;
 
 function Cabecalho() {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Olá React! 😎</h1>
       <hr />
       <Menu />
-    </header>
+    </StyledCabecalho>
   );
 }
 
