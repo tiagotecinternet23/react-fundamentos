ac58ad7fd2aa917d30a59674bc7d6ac3711f1467
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-01 11:11:09 -0300

    Correção Exercício 1

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,11 +1,26 @@
+import styled from "styled-components";
+
+const StyledArtigo = styled.article`
+  background-color: lavender;
+  padding: 1rem;
+  margin: 0.5rem 0;
+
+  h3 {
+    color: purple;
+    border-bottom: 1px solid;
+  }
+
+  p {
+    font-size: 1.1rem;
+  }
+`;
+
 function Artigo() {
   return (
-    <>
-      <article>
-        <h3>Artigo 1</h3>
-        <p>Conteúdo do artigo 1....</p>
-      </article>
-    </>
+    <StyledArtigo>
+      <h3>Artigo 1</h3>
+      <p>Conteúdo do artigo 1....</p>
+    </StyledArtigo>
   );
 }
 
