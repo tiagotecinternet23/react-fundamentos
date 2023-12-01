907629861913543956f971dc293a3816c304ed88
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-01 11:47:38 -0300

    Separação do StyledRodape

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,13 +1,4 @@
-import styled from "styled-components";
-
-const StyledRodape = styled.footer`
-  background-image: linear-gradient(pink, black);
-  color: white;
-  height: 30vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { StyledRodape } from "../styles/styles";
 
 function Rodape() {
   return (
