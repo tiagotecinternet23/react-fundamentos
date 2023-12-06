e1106168d275e2b9bd1ef0a80ec586c64ec4adf0
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-06 11:57:34 -0300

    Update Artigo.jsx

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+function Artigo(props) {
+  const [cor, setCor] = useState("lavender");
+
+  const mudarCor = () => {
+    setCor(cor === "lavender" ? "yellow" : "lavender");
+  };
+
+  const formatarPreco = (valor) => {
+    return valor.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
+
+  return (
+    <StyledArtigo onClick={mudarCor} style={{ backgroundColor: cor }}>
+      <h3> {props.categoria} </h3>
+      <p>
+        <b>Curso:</b> {props.titulo}
+      </p>
+      <p>
+        <b>Preço:</b> {formatarPreco(props.preco)}
+      </p>
+    </StyledArtigo>
+  );
+}
 
 const StyledArtigo = styled.article`
   background-color: lavender;
@@ -18,26 +46,5 @@ const StyledArtigo = styled.article`
     text-align: center;
   }
 `;
-
-function Artigo(props) {
-  const formatarPreco = (valor) => {
-    return valor.toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    });
-  };
-
-  return (
-    <StyledArtigo>
-      <h3> {props.categoria} </h3>
-      <p>
-        <b>Curso:</b> {props.titulo}
-      </p>
-      <p>
-        <b>Preço:</b> {formatarPreco(props.preco)}
-      </p>
-    </StyledArtigo>
-  );
-}
 
 export default Artigo;
