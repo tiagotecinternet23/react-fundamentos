49e0d12308396a373b0f754a8dcebc7bad800ae5
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-12-07 08:48:15 -0300

    Evento click nos botões

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,8 +1,44 @@
-/* Importação dos dados da "api de mentirinha" */
 import cursos from "../api/cursos";
-
 import Artigo from "./Artigo";
 import styled from "styled-components";
+
+function Conteudo() {
+  const aplicarFiltro = (event) => {
+    console.log(event.currentTarget.innerText);
+  };
+  return (
+    <StyledConteudo>
+      <h2>Conteúdo da aplicação</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        labore? Officia quae quo a quas excepturi distinctio sint voluptas
+        labore iste veniam possimus facere adipisci sit repellat, voluptate,
+        expedita aspernatur.
+      </p>
+
+      <div className="filtros">
+        <p>
+          <b>Filtrar por: </b>
+          <button onClick={aplicarFiltro}>Front-End</button>
+          <button onClick={aplicarFiltro}>Back-End</button>
+          <button onClick={aplicarFiltro}>Design</button>
+        </p>
+      </div>
+
+      <div className="artigos">
+        {cursos.map((curso) => (
+          <Artigo
+            key={curso.id}
+            categoria={curso.categoria}
+            titulo={curso.titulo}
+            preco={curso.preco}
+          />
+        ))}
+      </div>
+    </StyledConteudo>
+  );
+}
 
 const StyledConteudo = styled.main`
   width: 90vw;
@@ -14,6 +50,13 @@ const StyledConteudo = styled.main`
   h2,
   p {
     padding: 0.2rem 0;
+  }
+
+  .filtros {
+    margin: 1rem 0;
+    padding: 1rem 0;
+    border-top: solid 2px;
+    border-bottom: solid 2px;
   }
 
   @media screen and (min-width: 650px) {
@@ -29,31 +72,5 @@ const StyledConteudo = styled.main`
     }
   }
 `;
-
-function Conteudo() {
-  return (
-    <StyledConteudo>
-      <h2>Conteúdo da aplicação</h2>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        labore? Officia quae quo a quas excepturi distinctio sint voluptas
-        labore iste veniam possimus facere adipisci sit repellat, voluptate,
-        expedita aspernatur.
-      </p>
-
-      <div className="artigos">
-        {cursos.map((curso) => (
-          <Artigo
-            key={curso.id}
-            categoria={curso.categoria}
-            titulo={curso.titulo}
-            preco={curso.preco}
-          />
-        ))}
-      </div>
-    </StyledConteudo>
-  );
-}
 
 export default Conteudo;
