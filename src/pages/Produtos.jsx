a68f09472eebf70d438556b1680926b495d38aba
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-15 10:51:54 -0300

    Uso do fetch com API fake

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,16 @@
 function Produtos() {
+  const carregarDados = async () => {
+    try {
+      const resposta = await fetch(`https://fakestoreapi.com/products`);
+      const dados = await resposta.json();
+      console.log(dados);
+    } catch (error) {
+      console.error("Houve um erro: " + error);
+    }
+  };
+
+  carregarDados();
+
   return <h2>Produtos</h2>;
 }
 
