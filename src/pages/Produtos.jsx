0b07d492064c73af5c5476bce58a6c4fd1fe689c
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-15 12:01:03 -0300

    Loading

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -6,6 +6,9 @@ function Produtos() {
   ele será preenchido com os objetos/produtos. */
   const [produtos, setProdutos] = useState([]);
 
+  /* State de loading (por padrão, inicia ativado/true) */
+  const [loading, setLoading] = useState(true);
+
   /* Gerenciando o efeito colateral
   do componente para o carregamento dos dados da API. 
   
@@ -24,7 +27,8 @@ function Produtos() {
         const resposta = await fetch(`https://fakestoreapi.com/products`);
         const dados = await resposta.json();
         console.log(dados);
-        setProdutos(dados);
+        setProdutos(dados); // adicionamos ao state
+        setLoading(false); // desativamos o loading
       } catch (error) {
         console.error("Houve um erro: " + error);
       }
@@ -36,15 +40,19 @@ function Produtos() {
     <article>
       <h2>Produtos</h2>
 
-      {produtos.map((produto) => {
-        return (
-          <section key={produto.id}>
-            <h3> {produto.title} </h3>
-            <p>Preço: {produto.price}</p>
-            <p>{produto.description}</p>
-          </section>
-        );
-      })}
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        produtos.map((produto) => {
+          return (
+            <section key={produto.id}>
+              <h3> {produto.title} </h3>
+              <p>Preço: {produto.price}</p>
+              <p>{produto.description}</p>
+            </section>
+          );
+        })
+      )}
     </article>
   );
 }
