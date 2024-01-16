431a5cec4cea63882338b2c24612533b83b113f3
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-16 09:18:39 -0300

    Uso do useParams

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,4 +1,11 @@
+import { useParams } from "react-router-dom";
+
 function DetalhesProduto() {
+  /* Usamos o hook useParams do React Router DOM
+  para ter acesso aos parâmetros da rota dinâmica 
+  neste caso, o parâmetro chamado "id". */
+  const { id } = useParams();
+
   return (
     <article>
       <h2>Título...</h2>
